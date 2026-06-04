f2d4eff02816c42a82ae0b05cd6d4807bb2b06b2
luisfernando <luisfernandomsmg@gmail.com>
2026-06-04 14:20:20 -0600

    Actualizar lógica de JWT en el login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,12 +1,12 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const login = async (req, res) => {
   // Aquí 'user' recibe el texto "wixo3d" desde el frontend
   const { user, password } = req.body;
 
   try {
-    // CAMBIO AQUÍ: Usamos un nombre diferente (usuarioEncontrado) para no hacer cortocircuito
     const usuarioEncontrado = await User.findOne({ username: user?.trim() });
 
     if (!usuarioEncontrado) {
@@ -15,7 +15,6 @@ const login = async (req, res) => {
         .json({ success: false, message: "Usuario o contraseña incorrectos" });
     }
 
-    // CAMBIO AQUÍ: Comparamos contra la contraseña de 'usuarioEncontrado'
     const isMatch = await bcrypt.compare(
       password?.trim(),
       usuarioEncontrado.password,
@@ -30,9 +29,15 @@ const login = async (req, res) => {
     const unaSemanaEnMilisegundos = 7 * 24 * 60 * 60 * 1000;
     const expiryTime = new Date().getTime() + unaSemanaEnMilisegundos;
 
+    const tokenDinamico = jwt.sign(
+      { id: usuarioEncontrado._id, username: usuarioEncontrado.username },
+      process.env.JWT_SECRET, // Una clave ultra secreta que solo tu backend conoce
+      { expiresIn: "7d" }, // Define que expira en 7 días de forma nativa
+    );
+
     return res.json({
       success: true,
-      token: "wixo-secure-admin-session-token",
+      token: tokenDinamico, // <-- El token ahora sería una cadena gigante y única de caracteres encriptados
       expiry: expiryTime,
     });
   } catch (error) {
